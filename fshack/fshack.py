93bc52ef0accb73f593fdf77f74656b251db5b96
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2020-06-19 12:44:01 -0400

    Some medium-ish refactoring... improve inline help and job running/stdout/stderr/return code capture

--- fshack/fshack.py
+++ fshack/fshack.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python                                            
+#!/usr/bin/env python
 #
-# fshack ds ChRIS plugin app
+# fshack DS ChRIS plugin app
 #
 # (c) 2016-2020 Fetal-Neonatal Neuroimaging & Developmental Science Center
 #                   Boston Children's Hospital
@@ -12,6 +12,7 @@
 
 import os
 import sys
+import subprocess
 
 sys.path.append(os.path.dirname(__file__))
 
@@ -21,24 +22,26 @@ import pudb
 
 Gstr_title = """
 
-  __     _                _    
- / _|   | |              | |   
-| |_ ___| |__   __ _  ___| | __
-|  _/ __| '_ \ / _` |/ __| |/ /
-| | \__ \ | | | (_| | (__|   < 
-|_| |___/_| |_|\__,_|\___|_|\_\
-                               
+  __     _                _
+ / _|   | |              | |
+| |_ ___| |__   __ _  ___| | __  _ __  _   _
+|  _/ __| '_ \ / _` |/ __| |/ / | '_ \| | | |
+| | \__ \ | | | (_| | (__|   < _| |_) | |_| |
+|_| |___/_| |_|\__,_|\___|_|\_(_) .__/ \__, |
+                                | |     __/ |
+                                |_|    |___/
+
 
 """
 
 Gstr_synopsis = """
     NAME
 
-       fshack.py 
+       fshack.py
 
     SYNOPSIS
 
-        python fshack.py                                              \\
+        python fshack.py                                                \\
             -i|--inputFile <inputFileWithinInputDir>                    \\
             -o|--outputFile <outputFileWithinOutputDir>                 \\
             [-e|--exec <commandToExec>]                                 \\
@@ -55,43 +58,60 @@ Gstr_synopsis = """
 
     DESCRIPTION
 
-        This program is meant to be run inside of the container. To run with
-        Docker, copy this command and modify as needed:
+        This ChRIS DS plugin contains a complete FreeSurfer
 
-        docker run -v <pathToInput>:/incoming -v <pathToOutput>:/outgoing 
-		fnndsc/pl-fshack fshack.py -i myInputFile -o myOutputFiles /incoming /outgoing
+                https://surfer.nmr.mgh.harvard.edu/fswiki/rel7downloadsversion
 
-        <pathToInput> is the path to your input files
-        <pathToOutput> is the path to where you want your output to go
+        distribution. Not all FreeSurfer internal applications are exposed at
+        the plugin level, however. At time of writing, the following FreeSurfer
+        applications are directly accessible from the plugin CLI:
 
-        The path must be an absolute path (in other words, just a specific path).
+            * recon-all
+            * mri_convert
+            * mri_info
+            * mris_info
 
-        Assuming you're on a Windows operating system, this is what it might look
-        like:
+        This plugin is meant to demonstrate some design patterns as well
+        as providing some utility for running FreeSurfer within the context
+        of ChRIS. It is not meant nor intended to be a canonical FreeSurfer
+        ChRIS plugin -- as explicitly indicated by the name, FreeSurfer "hack",
+        `fshack`. Colloquially, however, this plugin is also known as `f-shack`.
 
-        Example:
+    ARGS
 
-        docker run -v /home/user/desktop:/incoming -v /home/user/desktop:/outgoing
-		fnndsc/pl-fshack fshack.py 
-		-i myInputFile -o myOutputFiles --exec recon-all /incoming /outgoing 
-
-    	ARGS
-
-	-i|--inputFile <inputFileWithinInputDir>
-        Input file to convert present in inputDir. Typically a DICOM file or a nifti volume.
+        -i|--inputFile <inputFileWithinInputDir>
+        Input file to process. In most cases this is typically a DICOM file
+        or a nifti volume, but is also very dependent on context. This file
+        exists within the explictly provided CLI positional <inputDir>.
 
         -o|--outputFile <outputFileWithinOutputDir>
         Output file/directory name to store the output in within the outputDir.
-        Note: This argument is used in place of -s|--subjectID <subjID> of FreeSurfer
+        Note: In the case of `recon-all`, this argument maps to the
+
+                -s|--subjectID <subjID>
+
+        CLI flag. This file is specified relative to the explicitly provided
+        positional CLI <outputDir>.
 
         [-e|--exec <commandToExec>]
-        Specifies the Freesurfer command to execute.
+        Specifies the FreeSurfer command within the plugin/container to
+        execute.
+
+        Note that only a few of the FreeSurfer apps are currently exposed!
 
         [-a|--args <argsPassedToExec>]
-        Specifies all the arguments within quotes (''), that FreeSurfer's commands:
-        recon-all, mri_convert, mri_info, and mris_info accepts.
-        Note: Pass the arguments as a string prefixed with 'ARGS: '
-        within single quotes 'ARGS:'
+        Optional string of additional arguments to "pass through" to the
+        FreeSurfer app.
+
+        The design pattern of this plugin is to provide all the CLI args for
+        a single app specificed `-exec` somewhat blindly. To this end, all the
+        args for a given internal FreeSurfer app are themselves specified at
+        the plugin level with this flag. These args MUST be contained within
+        single quotes (to protect them from the shell) and the quoted string
+        MUST start with the required keyword 'ARGS: '.
+
+        If the `--exec <FSapp>` does not require additional CLI args, then
+        this `--args <args>` can be safely omitted.
 
         [-h] [--help]
         If specified, show help message.
@@ -116,29 +136,29 @@ Gstr_synopsis = """
 
 
 class Fshack(ChrisApp):
-    """
-    This app will house a complete FreeSurfer install and run it via the plugin.
-    """
-    AUTHORS = 'FNNDSC (dev@babyMRI.org)'
-    SELFPATH = os.path.dirname(os.path.abspath(__file__))
-    SELFEXEC = os.path.basename(__file__)
-    EXECSHELL = 'python3'
-    TITLE = 'A quick-n-dirty attempt at hacking a FreeSurfer ChRIS plugin'
-    CATEGORY = ''
-    TYPE = 'ds'
-    DESCRIPTION = 'This app will house a complete FreeSurfer install and run it via the plugin'
-    DOCUMENTATION = 'http://wiki'
-    VERSION = '0.1'
-    ICON = ''  # url of an icon image
-    LICENSE = 'Opensource (MIT)'
-    MAX_NUMBER_OF_WORKERS = 1  # Override with integer value
-    MIN_NUMBER_OF_WORKERS = 1  # Override with integer value
-    MAX_CPU_LIMIT = ''  # Override with millicore value as string, e.g. '2000m'
-    MIN_CPU_LIMIT = ''  # Override with millicore value as string, e.g. '2000m'
-    MAX_MEMORY_LIMIT = ''  # Override with string, e.g. '1Gi', '2000Mi'
-    MIN_MEMORY_LIMIT = ''  # Override with string, e.g. '1Gi', '2000Mi'
-    MIN_GPU_LIMIT = 0  # Override with the minimum number of GPUs, as an integer, for your plugin
-    MAX_GPU_LIMIT = 0  # Override with the maximum number of GPUs, as an integer, for your plugin
+    DESCRIPTION  = '''
+        This app houses a complete FreeSurfer distro and exposes some
+        FreeSurfer apps at the level of the plugin CLI.'
+    '''
+    AUTHORS                 = 'FNNDSC (dev@babyMRI.org)'
+    SELFPATH                = os.path.dirname(os.path.abspath(__file__))
+    SELFEXEC                = os.path.basename(__file__)
+    EXECSHELL               = 'python3'
+    TITLE                   = 'A quick-n-dirty attempt at hacking a FreeSurfer ChRIS plugin'
+    CATEGORY                = ''
+    TYPE                    = 'ds'
+    DOCUMENTATION           = 'https://github.com/FNNDSC/pl-fshack'
+    VERSION                 = '1.0'
+    ICON                    = ''  # url of an icon image
+    LICENSE                 = 'Opensource (MIT)'
+    MAX_NUMBER_OF_WORKERS   = 1  # Override with integer value
+    MIN_NUMBER_OF_WORKERS   = 1  # Override with integer value
+    MAX_CPU_LIMIT           = ''  # Override with millicore value as string, e.g. '2000m'
+    MIN_CPU_LIMIT           = ''  # Override with millicore value as string, e.g. '2000m'
+    MAX_MEMORY_LIMIT        = ''  # Override with string, e.g. '1Gi', '2000Mi'
+    MIN_MEMORY_LIMIT        = ''  # Override with string, e.g. '1Gi', '2000Mi'
+    MIN_GPU_LIMIT           = 0  # Override with the minimum number of GPUs, as an integer, for your plugin
+    MAX_GPU_LIMIT           = 0  # Override with the maximum number of GPUs, as an integer, for your plugin
 
     # Use this dictionary structure to provide key-value output descriptive information
     # that may be useful for the next downstream plugin. For example:
@@ -156,14 +176,7 @@ class Fshack(ChrisApp):
     def define_parameters(self):
         """
         Define the CLI arguments accepted by this plugin app.
-        Use self.add_argument to specify a new app argument.
         """
-        # self.add_argument("-s", "--subjectID",
-        #                     help        = "The subject directory",
-        #                     type        = str,
-        #                     dest        = 'subjectID',
-        #                     optional    = True,
-        #                     default     = "")
         self.add_argument("-a", "--args",
                           help="FS arguments to pass",
                           type=str,
@@ -186,15 +199,68 @@ class Fshack(ChrisApp):
                           help="output file",
                           type=str,
                           dest='outputFile',
-                          optional=False,
+                          optional=True,
                           default="")
 
-    def get_first_file(self, directory):
-        for file in os.listdir(directory):
-            if file.endswith(".dcm"):
-                return file
-            elif file.endswith(".nii"):
-                return file
+    def job_run(self, str_cmd):
+        """
+        Running CLI process via python is cumbersome. The typical/easy path
+        of
+
+                os.system(str_cmd)
+
+        is deprecated and prone to hidden complexity. The preferred
+        method is via subprocess, which has a cumbersome processing
+        syntax. Still, this method runs the `str_cmd` and returns the
+        stderr and stdout strings as well as a returncode.
+
+        Providing readtime output of both stdout and stderr seems
+        problematic. The approach here is to provide realtime
+        output on stdout and only provide stderr on prcoess completion.
+
+        """
+        d_ret = {
+            'stdout':       "",
+            'stderr':       "",
+            'returncode':   0
+        }
+
+        p = subprocess.Popen(
+                    str_cmd.split(),
+                    stdout      = subprocess.PIPE,
+                    stderr      = subprocess.PIPE,
+        )
+
+        # Realtime output on stdout
+        str_stdoutLine  = ""
+        str_stdout      = ""
+        while True:
+            stdout      = p.stdout.readline()
+            if p.poll() is not None:
+                break
+            if stdout:
+                str_stdoutLine = stdout.decode()
+                print(str_stdoutLine, end = '')
+                str_stdout      += str_stdoutLine
+        d_ret['stdout']     = str_stdout
+        d_ret['stderr']     = p.stderr.read().decode()
+        d_ret['returncode'] = p.returncode
+        print('\nstderr: \n%s' % d_ret['stderr'])
+        return d_ret
+
+    def job_stdwrite(self, d_job, options):
+        """
+        Capture the d_job entries to respective files.
+        """
+        for key in d_job.keys():
+            with open(
+                '%s/%s-%s' % (options.outputdir, options.outputFile, key), "w"
+            ) as f:
+                f.write(str(d_job[key]))
+                f.close()
+        return {
+            'status': True
+        }
 
     def run(self, options):
         """
@@ -204,36 +270,44 @@ class Fshack(ChrisApp):
         print(Gstr_title)
         print('Version: %s' % self.get_version())
 
-        # get first file inside of the directory
-        str_inputFile = self.get_first_file(options.inputdir)
-
-        l_appargs = options.args.split('ARGS:')
+        str_args    = ""
+        l_appargs   = options.args.split('ARGS:')
         if len(l_appargs) == 2:
             str_args = l_appargs[1]
         else:
             str_args = l_appargs[0]
 
-        if options.exec == 'recon-all':  # -all -notalairach -parallel -openmp %d
-            str_cmd = '/usr/local/freesurfer/bin/%s -i %s/%s -subjid %s/%s %s' % \
-                      (options.exec, options.inputdir, options.inputFile,
+        str_FSbinDir    = '/usr/local/freesurfer/bin'
+        str_cmd         = ""
+        if options.exec == 'recon-all':
+            str_cmd = '%s/%s -i %s/%s -subjid %s/%s %s ' % \
+                      (str_FSbinDir,
+                       options.exec, options.inputdir, options.inputFile,
                        options.outputdir, options.outputFile, str_args)
 
-        if options.exec == 'mri_convert':  # --split -zo
-            str_cmd = '/usr/local/freesurfer/bin/%s %s/%s  %s/%s %s' % \
-                      (options.exec, options.inputdir, options.inputFile,
+        if options.exec == 'mri_convert':
+            str_cmd = '%s/%s %s/%s  %s/%s %s ' % \
+                      (str_FSbinDir,
+                       options.exec, options.inputdir, options.inputFile,
                        options.outputdir, options.outputFile, str_args)
 
-        if options.exec == 'mri_info':  # --conformed --type
-            str_cmd = '/usr/local/freesurfer/bin/%s %s/%s %s > %s/%s' % \
-                      (options.exec, options.inputdir, options.inputFile,
-                       str_args, options.outputdir, options.outputFile)
+        if options.exec == 'mri_info':
+            str_cmd = '%s/%s %s/%s %s ' % \
+                      (str_FSbinDir,
+                       options.exec, options.inputdir, options.inputFile,
+                       str_args)
 
-        if options.exec == 'mris_info':  # --ncols #nrows
-            str_cmd = '/usr/local/freesurfer/bin/%s %s/%s %s > %s/%s' % \
-                      (options.exec, options.inputdir, options.inputFile,
-                       str_args, options.outputdir, options.outputFile)
-        # pudb.set_trace()
-        os.system(str_cmd)
+        if options.exec == 'mris_info':
+            str_cmd = '%s/%s %s/%s %s' % \
+                      (str_FSbinDir,
+                       options.exec, options.inputdir, options.inputFile,
+                       str_args)
+
+        # Run the job and provide realtime stdout
+        # and post-run stderr
+        self.job_stdwrite(
+            self.job_run(str_cmd), options
+        )
 
     def show_man_page(self):
         """
